3c07a2facf35eb652bef5428c69e16d54a4cb09a
lbj <1195784202@qq.com>
2025-10-30 03:02:03 +0800

    write menu and playground page

--- game/static/js/src/zbase.js
+++ game/static/js/src/zbase.js
@@ -1,4 +1,11 @@
 class AcGame{
     constructor(id){
+        this.id = id;
+        this.$ac_game = $('#' + id);        // 找到html里定义的主要对象(大div块)，取名为ac_game对象
+        this.menu = new AcGameMenu(this);
+        this.playground = new AcGamePlayground(this);
+        
+        this.start();
     }
+    start(){}
 }
