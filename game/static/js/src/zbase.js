be6492c35f533ecb663012126ba10ebf557a5e47
lbj <1195784202@qq.com>
2025-11-02 15:48:11 +0800

    Implement Single-player-mode

--- game/static/js/src/zbase.js
+++ game/static/js/src/zbase.js
@@ -1,8 +1,8 @@
-class AcGame{
+export class AcGame{
     constructor(id){
         this.id = id;
         this.$ac_game = $('#' + id);        // 找到html里定义的主要对象(大div块)，取名为ac_game对象
-        this.menu = new AcGameMenu(this);
+        // debug - this.menu = new AcGameMenu(this);
         this.playground = new AcGamePlayground(this);
         
         this.start();
