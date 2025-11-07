004ec42844ad21f43b9ecef2c8c04ab8ab5a2422
lbj <1195784202@qq.com>
2025-11-07 18:47:28 +0800

    Normalize unit

--- static/js/src/playground/game_map/zbase.js
+++ static/js/src/playground/game_map/zbase.js
@@ -9,6 +9,12 @@ class GameMap extends AcGameObject{
         this.playground.$playground.append(this.$canvas);
     }
     start(){}
+    resize(){       // 根据playground的变化调整画布大小
+        this.ctx.canvas.width = this.playground.width;
+        this.ctx.canvas.height = this.playground.height;
+        this.ctx.fillStyle = "rgba(0, 0, 0, 1)";
+        this.ctx.fillRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+    }
     update(){
         this.render();
     }
